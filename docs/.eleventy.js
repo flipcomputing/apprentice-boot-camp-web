30d63567ecc63d52f8cc511e5a6a7bbf0ad2cda0
TomSteeples <thomas.steeples@zuto.com>
2022-04-22 16:22:10 +0100

    slides first test

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/images/')
 
   return {
+    // pathPrefix: "/public/slides/slide_000",
     dir: {
       input: "src",
       output: "public"
